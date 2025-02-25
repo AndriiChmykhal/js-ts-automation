95faa8cb3b5a1b6f3fc1c74bcc1bec99dae5946a
Andrii Chmykhal <andrii.chmykhal@gmail.com>
2025-02-25 20:36:52 +0000

    added access modifiers

--- lesson8/src/abstraction.ts
+++ lesson8/src/abstraction.ts
@@ -1,40 +1,40 @@
 abstract class Person {
-    name: String;
-    email: String;
+    protected name: string;
+    protected email: string;
 
-    constructor(name: string, email: string) {
+    public constructor(name: string, email: string) {
         this.name = name;
         this.email = email;
     }
 
-    abstract displayInfo(): String;
+    protected abstract displayInfo(): string;
 }
 
 export class FullAddress {
-    street: string;
-    city: string;
-    zipcode: string;
+    protected street: string;
+    protected city: string;
+    protected zipcode: string;
 
-    constructor(street: string, city: string, zipcode: string) {
+    public constructor(street: string, city: string, zipcode: string) {
         this.street = street;
         this.city = city;
         this.zipcode = zipcode;
     }
 
-    getFullAddress(): string {
+    public getFullAddress(): string {
         return `${this.street}, ${this.city}, ${this.zipcode}`;
     }
 }
 
 export class UserDetails extends Person {
-    fullAddress: FullAddress;
+    protected fullAddress: FullAddress;
 
-    constructor(name: string, email: string, fullAddress: FullAddress) {
+    public constructor(name: string, email: string, fullAddress: FullAddress) {
         super(name, email);
         this.fullAddress = fullAddress;
     }
 
-    displayInfo(): string {
+    public displayInfo(): string {
         return `User: ${this.name}, Email: ${this.email}, Address: ${this.fullAddress.getFullAddress()}`;
     }
 }
